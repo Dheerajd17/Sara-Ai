f80c83b1ce60bcd803b889abd417eba510d68593
DHEERAJ <121352878+Dheerajd17@users.noreply.github.com>
2024-10-05 15:39:27 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,2 @@
+music={"gangster":"https://www.youtube.com/watch?v=fPO76Jlnz6c","skyfall":"https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D","wolf":"https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+}
